19e1ad4b0b28820f2cac72924279ab3d8a6d4d8e
EliteIvan96 <edgar.ivanmagallanes@gmail.com>
2026-03-06 18:02:48 -0600

    Revision ejercicio 5 eliminacion de elemantos de un Diccionario

--- Clase2/ejercicio2.py
+++ Clase2/ejercicio2.py
@@ -4,25 +4,19 @@ estudiantes = {
     'A003': { "nombre": "ESMERALDA", "edad": 20, "promedio": 4.5 }
 }
 
-#PARTE 1 — INSERTAR DATOS
-#1.- Agrega manualmente al menos 3 estudiantes al diccionario.
-#2.- Imprime el diccionario completo.
-# INSERTAR UN NUEVO ELEMENTO EN EL DICCIONARIO
 
-#paises_capitales['Chile'] = 'Santiago de Chile'
+"""PARTE 5 — ELIMINAR ESTUDIANTE
 
-estudiantes["A004"] = { "nombre": "JUAN","edad": 14,"promedio": 6.5 }
-estudiantes["A005"] = { "nombre": "MIGUEL","edad": 30,"promedio": 4.5 }
-estudiantes["A006"] = { "nombre": "FELIPE","edad": 35,"promedio": 8.5 }
-#print(estudiantes)
+1.- Solicita una matrícula.
+2.- Si existe, elimínala del diccionario usando del.
+3.- Muestra el diccionario final.
+"""
 
-#for mat, val in estudiantes.items():
- #print(f"MATRICULA: {mat}: {val['nombre']}, EDAD: {val['edad']}, PROMEDIO: {val['promedio']}")
+mat = input("ingresa la matricula :")
+del estudiantes[mat]
+print(estudiantes) 
 
-print(f"Encuentra el alumno con mayor promedio ")
-promedio = max(estudiantes[mat]["promedio"] for mat in estudiantes)
-print("El mayor promedio es:", promedio)
-   
+
 
 
 
