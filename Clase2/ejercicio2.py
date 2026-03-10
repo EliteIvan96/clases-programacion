9424476a7b4e6bbab73f2e618e21e1b9152c7fc4
EliteIvan96 <edgar.ivanmagallanes@gmail.com>
2026-03-09 18:03:18 -0600

    Se optimiza solucion del ejercicio 6 obtener el mayor promedio y el promedio general

--- Clase2/ejercicio2.py
+++ Clase2/ejercicio2.py
@@ -1,21 +1,28 @@
+
+"""PARTE 6 — ANÁLISIS
+
+1.- Encuentra el estudiante con mayor promedio.
+2.- Calcula el promedio general del grupo.
+3.- Muestra los resultados en pantalla."""
+
 estudiantes = {
-    'A001': { "nombre": "EDGAR", "edad": 23, "promedio": 8.5 },
+    'A001': { "nombre": "EDGAR", "edad": 23, "promedio":2.2 },
     'A002': { "nombre": "KAREN", "edad": 29, "promedio": 7.5 },
     'A003': { "nombre": "ESMERALDA", "edad": 20, "promedio": 4.5 }
 }
+# Variables para el mayor promedio
+mayor_promedio = 0
+# Recorrer el diccionario
+for matricula in estudiantes:
+    promedio = estudiantes[matricula]["promedio"]
 
+    if promedio > mayor_promedio:
+        mayor_promedio = promedio
+       # print(promedio)
 
-"""PARTE 5 — ELIMINAR ESTUDIANTE
-
-1.- Solicita una matrícula.
-2.- Si existe, elimínala del diccionario usando del.
-3.- Muestra el diccionario final.
-"""
-
-mat = input("ingresa la matricula :")
-del estudiantes[mat]
-print(estudiantes) 
-
+suma = estudiantes['A001']['promedio'] + estudiantes['A002']['promedio'] + estudiantes['A003']['promedio']
+print("El Mayor promedio de la clase es :", mayor_promedio)
+print("El promedio general del grupo es :", suma / len(estudiantes))
 
 
 
